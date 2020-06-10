372551f7662f634a0e69888489ff9ff34fe40553
JokerMV@outlook.com <JokerMV@outlook.com>
2020-06-10 21:39:22 +0700

    Initial solution and reponsetory

---
+++ Eshopsolution.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Eshopsolution.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
